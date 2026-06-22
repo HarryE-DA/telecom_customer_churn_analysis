41c79799e40ac8dfe7e7ac4b6aa4f2c554379dc8
Harry Evans <harryte29@gmail.com>
2026-06-22 10:55:08 +0100

    Update and rename Avg_monthly_charge_Vs_Churn.sql to 07_avg_monthly_charge_vs_churn.sql

--- sql/07_avg_monthly_charge_vs_churn.sql
+++ sql/07_avg_monthly_charge_vs_churn.sql
@@ -1,4 +1,4 @@
-SELECT ROUND(AVG(monthly_charge),2) AS avg_monthly_charge,
+ SELECT ROUND(AVG(monthly_charge),2) AS avg_monthly_charge,
 COUNT (*) AS total_customers,
 SUM(CASE WHEN churn = TRUE THEN 1 ELSE 0 END) AS churned_customers,
 ROUND(SUM(CASE WHEN churn = TRUE THEN 1 ELSE 0 END) *100.0/COUNT(*),2) AS churn_rate
